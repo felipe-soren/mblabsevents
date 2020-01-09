76a9b57482ff7e05262a1656ed5fac73217f352b
unknown <Felipe Novaes>
2020-01-09 09:34:03 -0300

    Adicionando rotas de login e cadastro de usuario

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.png'
 
+import { Link } from "react-router-dom";
 import { Container } from './styles';
 
 export default class header extends Component {
   render() {
     return ( 
       <Container>
-        <a href="teste">Home</a>
+        <a href="/app">Home</a>
 
         <nav>
-          <li><a href="teste">Home</a></li>
-          <li><a href="teste">Login</a></li>
+          <li><a href="/sigin">Login</a></li>
+          <li><a href="/signup">Cadastre-se</a></li>
         </nav>
       </Container>
     );
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,19 +1,21 @@
 import { createGlobalStyle } from 'styled-components'
+import "font-awesome/css/font-awesome.css";
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    outline: 0;
-    text-decoration: none;
-
-    body {
-      text-rendering: optimizeLegibility !important;
-      -webkit-font-smoothing: antialiased !important;
-      font-family: sans-serif;
-    }
-  }
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  outline: 0;
+}
+body, html {
+  background: #eee;
+  font-family: 'Helvetica Neue', 'Helvetica', Arial, sans-serif;
+  text-rendering: optimizeLegibility !important;
+  -webkit-font-smoothing: antialiased !important;
+  height: 100%;
+  width: 100%;
+}
 `;
 
 export default GlobalStyle;
